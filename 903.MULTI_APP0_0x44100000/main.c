1af08de9723a3ad8d37c598d5278ce9b7321d608
parkdoyun <ehdqkdfjq97@gmail.com>
2024-07-17 14:21:31 +0900

    [parkdoyun] Make application 1

--- 903.MULTI_APP0_0x44100000/main.c
+++ 903.MULTI_APP0_0x44100000/main.c
@@ -6,9 +6,13 @@
 #include ".\images\img01.h"
 #include ".\images\how_to_play.h"
 #include ".\images\map1.h"
+#include ".\images\map2.h"
 #include ".\images\ghost_red.h"
+#include ".\images\ghost_green.h"
 #include ".\images\block_black.h"
+#include ".\images\block_black2.h"
 #include ".\images\block_blue.h"
+#include ".\images\block_blue2.h"
 #include ".\images\success.h"
 #include ".\images\Logo.h"
 
@@ -27,16 +31,157 @@ extern WIN_INFO_ST ArrWinInfo[5];
 #define DELAY	8000
 
 #define startX 0
-#define startY 50
-#define goalX 900
-#define goalY 550
-#define step_size 50
 #define LCD_width 1000
 #define LCD_height 600
 
-const unsigned short * img[]={Logo, block_black, nct127_walk, ghost_red, how_to_play, block_blue};
-const unsigned short * map[] = {map1};
-const unsigned short * popup[] = {success};
+const unsigned short * img[]={Logo, nct127_walk};
+const unsigned short * map[] = {map1, map2};
+const unsigned short * user[] = {ghost_red, ghost_green};
+const unsigned short * block_blk[] = {block_black, block_black2};
+const unsigned short * block_blu[] = {block_blue, block_blue2};
+const unsigned short * popup[] = {success, how_to_play};
+int px, py, xtmp, ytmp, step_size;
+int startY, goalX, goalY;
+unsigned char dir;
+
+void var_init(int s_size)
+{
+	px = 0;
+	py = s_size;
+	startY = s_size;
+	goalY = 600 - s_size;
+	goalX = 1000 - s_size * 2;
+	step_size = s_size;
+}
+
+void game(int idx)
+{
+	Lcd_Clr_Screen();
+	Lcd_Draw_BMP(0,0,map[idx]); // img 출력
+
+	// 그 위에 작은 사진 올릴 수 있는지?
+	Lcd_Draw_BMP(startX,startY,user[idx]);
+	int map_arr[25][41];
+
+	int i, j;
+	if(idx == 0)
+	{
+		for(i = 0; i < 13; i++)
+		{
+			for(j = 0; j < 21; j++)
+			{
+				map_arr[i][j] = map1_arr[i][j];
+			}
+		}
+	}
+	else if(idx == 1)
+	{
+		for(i = 0; i < 25; i++)
+		{
+			for(j = 0; j < 41; j++) map_arr[i][j] = map2_arr[i][j];
+		}
+	}
+	else
+	{
+		Uart_Printf("\nInvalid index.\n");
+		return;
+	}
+
+		// stage 1
+		for(;;)
+		{
+			if(py == goalY && px == goalX)
+			{
+				Lcd_Draw_BMP(100,100,popup[0]);
+				break;
+			}
+
+			Uart_Printf("\nEnter direction key.\n");
+			dir = Uart1_Get_Char(); // 방향키는 특수 키라 두번 입력받아야 함 -> 27 91로 똑같다, WASD space로 동작
+			//dir2 = Uart1_Get_Char();
+
+
+			if(dir == 119)
+			{
+				Uart_Printf("UP\n");
+				//Lcd_Clr_Screen();
+				if(((py - step_size) < 0) || (map_arr[(py - step_size) / step_size][px / step_size] == 0)) continue;
+				//Lcd_Draw_BMP(0,0,map[0]);
+				py -= step_size;
+				if(py < startY) py = startY;
+				else Lcd_Draw_BMP(px, py + step_size, block_blk[idx]);
+				Lcd_Draw_BMP(px,py,user[idx]);
+			}
+			else if(dir == 97)
+			{
+				Uart_Printf("LEFT\n");
+				//Lcd_Clr_Screen();
+				//Lcd_Draw_BMP(0,0,map[0]);
+				if(((px - step_size) < 0) || (map_arr[(py / step_size)][(px - step_size) / step_size] == 0)) continue;
+				px -= step_size;
+				if(px < startX) px = startX;
+				else Lcd_Draw_BMP(px + step_size, py, block_blk[idx]);
+				Lcd_Draw_BMP(px,py,user[idx]);
+			}
+			else if(dir == 115)
+			{
+				Uart_Printf("DOWN\n");
+				//Lcd_Clr_Screen();
+				//Lcd_Draw_BMP(0,0,map[0]);
+				if(((py + step_size) > LCD_height) || (map_arr[(py + step_size) / step_size][px / step_size] == 0)) continue;
+				py += step_size;
+				if(py > LCD_height) py = LCD_height;
+				else if ((py - step_size) == startY && px == startX) Lcd_Draw_BMP(px, py - step_size, block_blu[idx]);
+				else Lcd_Draw_BMP(px, py - step_size, block_blk[idx]);
+				Lcd_Draw_BMP(px,py,user[idx]);
+			}
+			else if(dir == 100)
+			{
+				Uart_Printf("RIGHT\n");
+				//Lcd_Clr_Screen();
+				//Lcd_Draw_BMP(0,0,map[0]);
+				if(((px + step_size) > LCD_width) || (map_arr[(py / step_size)][(px + step_size) / step_size] == 0)) continue;
+				px += step_size;
+				if(px > LCD_width) px = LCD_width;
+				else if ((px - step_size) == startX && py == startY) Lcd_Draw_BMP(px - step_size, py, block_blu[idx]);
+				else Lcd_Draw_BMP(px - step_size, py, block_blk[idx]);
+				Lcd_Draw_BMP(px,py,user[idx]);
+			}
+			else if(dir == 32)
+			{
+				Uart_Printf("SPACE\n");
+				//Lcd_Clr_Screen();
+				//Lcd_Draw_BMP(0,0,map[0]);
+				Lcd_Draw_BMP(px, py, block_blk[idx]);
+				py = startY;
+				px = startX;
+				Lcd_Draw_BMP(px,py,user[idx]);
+			}
+			else if(dir == 104)
+			{
+				Uart_Printf("HOWTOPLAY\n");
+				//Lcd_Clr_Screen();
+				Lcd_Draw_BMP(100,100,popup[1]);
+				for(;;)
+				{
+					// X 키로 나가기
+					Uart_Printf("\nEnter direction key.(EXIT)\n");
+					dir = Uart1_Get_Char(); // 방향키는 특수 키라 두번 입력받아야 함 -> 27 91로 똑같다, WASD space로 동작
+					if(dir == 120)
+					{
+						Lcd_Draw_BMP(0,0,map[idx]);
+						Lcd_Draw_BMP(px,py,user[idx]);
+						break;
+					}
+					else Uart_Printf("Input is invalid.[%d]\n", dir);
+				}
+			}
+			else Uart_Printf("Input is invalid.[%d]\n", dir);
+			Uart_Printf("py[%d], px[%d], y[%d], x[%d] %d\n", py, px, py / step_size, px / step_size, map_arr[py/step_size][px/step_size]);
+			//Delay(10);
+
+		}
+}
 
 void Main(void)
 {
@@ -54,133 +199,38 @@ void Main(void)
 	Lcd_Init();
 	Lcd_Win_Init(0, 1);
 	Lcd_Brightness_Control(8);
+	Key_Poll_Init();
 
 	Lcd_Select_Display_Frame_Buffer(0, 0);
 	Lcd_Select_Draw_Frame_Buffer(0, 0);
 	Lcd_Clr_Screen();
-
 	Lcd_Draw_BMP(0,0,img[0]); // img 출력
 
 	Key_Wait_Key_Released();
 	Key_Wait_Key_Pressed();
 
-	Lcd_Clr_Screen();
-
-	Lcd_Draw_BMP(0,0,map[0]); // img 출력
-
-	// 그 위에 작은 사진 올릴 수 있는지?
-	int px, py, xtmp, ytmp;
-	px = 0;
-	py = 50;
-	Lcd_Get_Info_BMP(&xtmp, &ytmp, img[3]);
+	// Stage 1
+	var_init(50);
+	game(0);
+	/*
+	Lcd_Get_Info_BMP(&xtmp, &ytmp, user[0]);
 
 	//Set_Frame_Buffer1_Cache(0);
 
-	memcpy((void *)LCD_FB41_START_ADDR, img[3], xtmp * ytmp * 2 + 8);
+	memcpy((void *)LCD_FB41_START_ADDR, user[0], xtmp * ytmp * 2 + 8);
 	//Set_Frame_Buffer1_Cache(mode);
 
 	//Timer1_Stopwatch_Run();
 	//if(mode == 4) Partial_Invalidate_Cache(LCD_FB41_START_ADDR, xtmp * ytmp * 2 + 8);
-	Lcd_Draw_BMP(px,py,img[3]);
+	Lcd_Draw_BMP(px,py,user[0]);
+	*/
+	Key_Wait_Key_Released();
+	Key_Wait_Key_Pressed();
 
-	// key 눌러서 물체 옮기기 가능한지?
-	unsigned char dir;
+	// stage 2
+	var_init(25);
+	game(1);
 
-	for(;;)
-	{
-		//Lcd_Draw_BMP(0,0,img[2]); // img 출력
-		//Delay(DELAY);
-		//Lcd_Clr_Screen();
+	for(;;){}
 
-		if(py == goalY && px == goalX)
-		{
-			Lcd_Draw_BMP(100,100,popup[0]);
-			for(;;){}
-		}
-
-		Uart_Printf("\nEnter direction key.\n");
-		dir = Uart1_Get_Char(); // 방향키는 특수 키라 두번 입력받아야 함 -> 27 91로 똑같다, WASD space로 동작
-		//dir2 = Uart1_Get_Char();
-
-
-		if(dir == 119)
-		{
-			Uart_Printf("UP\n");
-			//Lcd_Clr_Screen();
-			if(((py - step_size) < 0) || (map1_arr[(py - step_size) / step_size][px / step_size] == 0)) continue;
-			//Lcd_Draw_BMP(0,0,map[0]);
-			py -= step_size;
-			if(py < startY) py = startY;
-			else Lcd_Draw_BMP(px, py + step_size, img[1]);
-			Lcd_Draw_BMP(px,py,img[3]);
-		}
-		else if(dir == 97)
-		{
-			Uart_Printf("LEFT\n");
-			//Lcd_Clr_Screen();
-			//Lcd_Draw_BMP(0,0,map[0]);
-			if(((px - step_size) < 0) || (map1_arr[(py / step_size)][(px - step_size) / step_size] == 0)) continue;
-			px -= step_size;
-			if(px < startX) px = startX;
-			else Lcd_Draw_BMP(px + step_size, py, img[1]);
-			Lcd_Draw_BMP(px,py,img[3]);
-		}
-		else if(dir == 115)
-		{
-			Uart_Printf("DOWN\n");
-			//Lcd_Clr_Screen();
-			//Lcd_Draw_BMP(0,0,map[0]);
-			if(((py + step_size) > LCD_height) || (map1_arr[(py + step_size) / step_size][px / step_size] == 0)) continue;
-			py += step_size;
-			if(py > LCD_height) py = LCD_height;
-			else if ((py - step_size) == startY && px == startX) Lcd_Draw_BMP(px, py - step_size, img[5]);
-			else Lcd_Draw_BMP(px, py - step_size, img[1]);
-			Lcd_Draw_BMP(px,py,img[3]);
-		}
-		else if(dir == 100)
-		{
-			Uart_Printf("RIGHT\n");
-			//Lcd_Clr_Screen();
-			//Lcd_Draw_BMP(0,0,map[0]);
-			if(((px + step_size) > LCD_width) || (map1_arr[(py / step_size)][(px + step_size) / step_size] == 0)) continue;
-			px += step_size;
-			if(px > LCD_width) px = LCD_width;
-			else if ((px - step_size) == startX && py == startY) Lcd_Draw_BMP(px - step_size, py, img[5]);
-			else Lcd_Draw_BMP(px - step_size, py, img[1]);
-			Lcd_Draw_BMP(px,py,img[3]);
-		}
-		else if(dir == 32)
-		{
-			Uart_Printf("SPACE\n");
-			//Lcd_Clr_Screen();
-			//Lcd_Draw_BMP(0,0,map[0]);
-			Lcd_Draw_BMP(px, py, img[1]);
-			py = startY;
-			px = startX;
-			Lcd_Draw_BMP(px,py,img[3]);
-		}
-		else if(dir == 104)
-		{
-			Uart_Printf("HOWTOPLAY\n");
-			//Lcd_Clr_Screen();
-			Lcd_Draw_BMP(100,100,img[4]);
-			for(;;)
-			{
-				// X 키로 나가기
-				Uart_Printf("\nEnter direction key.(EXIT)\n");
-				dir = Uart1_Get_Char(); // 방향키는 특수 키라 두번 입력받아야 함 -> 27 91로 똑같다, WASD space로 동작
-				if(dir == 120)
-				{
-					Lcd_Draw_BMP(0,0,map[0]);
-					Lcd_Draw_BMP(px,py,img[3]);
-					break;
-				}
-				else Uart_Printf("Input is invalid.[%d]\n", dir);
-			}
-		}
-		else Uart_Printf("Input is invalid.[%d]\n", dir);
-		Uart_Printf("y[%d], x[%d] %d\n", py / step_size, px / step_size, map1_arr[py/step_size][px/step_size]);
-		//Delay(10);
-
-	}
 }
